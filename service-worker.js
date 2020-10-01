8e9a5554a90b9dde357293c65f1d688a39c44702
DSC-Amritapuri <dsc.amritapuri@gmail.com>
2020-10-01 03:15:25 +0000

    gh-pages

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+importScripts("/precache-manifest.66982fd59199efd983071946630df3a5.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+self.addEventListener('message', (e) => {
+    if (!e.data) {
+      return;
+    }
+  
+    switch (e.data) {
+      case 'skipWaiting':
+        self.skipWaiting();
+        break;
+      default:
+        // NOOP
+        break;
+    }
+  });
+  
+  var workbox;
+  workbox.clientsClaim();
+  
+  // The precaching code provided by Workbox.
+  self.__precacheManifest = [].concat(self.__precacheManifest || []);
+  workbox.precaching.suppressWarnings();
+  workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
